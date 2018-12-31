e4777c5ceddb4d434cdced33810075f1ae28ff50
Aaron Bini <bini.aaron@gmail.com>
2018-12-31 10:47:18 -0800

    initial implementation of the real-time TriMet transit map

--- web/index.js
+++ web/index.js
@@ -1,7 +1,52 @@
-var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
- 
-mapboxgl.accessToken = 'sk.eyJ1IjoiYWFyb25iaW5pIiwiYSI6ImNqcTl1dzNpcTBjZHAzeG1uY2hzY3lhaXAifQ.tyk2ZDM3iEKTX9Jsd5s0iA';
-var map = new mapboxgl.Map({
-container: 'YOUR_CONTAINER_ELEMENT_ID',
-style: 'mapbox://styles/mapbox/streets-v11'
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWFyb25iaW5pIiwiYSI6ImNpcHU3ajc2cjA5eGNmbG0yZmh2a2Fud3EifQ.bMYgeUt9yYRmG3Za0B9lSw';
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/light-v9',
+  zoom: 12,
+  center: [-122.6739923, 45.5163031]
 });
+let initialized = false;
+//mapbox marker array
+let mapLocations = [];
+function updateLocations(locationArray) {
+  locationArray.forEach((element, index) => {
+    if (mapLocations[index]) {
+      mapLocations[index].setLngLat([element.longitude, element.latitude]);
+    }
+  });
+}
+
+const host = window.document.location.host.replace(/:.*/, '');
+const ws = new WebSocket('ws://localhost:8080');
+
+function moveMarkers(event) {
+  let incoming = JSON.parse(event.data);
+  if (!initialized) {
+    initialized = true;
+    initializeMarkers(incoming);
+  } else {
+    updateLocations(incoming.resultSet.vehicle);
+  }
+}
+
+function initializeMarkers(incoming) {
+  incoming.resultSet.vehicle.forEach(location => {
+    let el = document.createElement('div');
+    el.className = 'dot';
+    if (location.type === 'bus') {
+      el.classList.add([ 'bus' ]);
+    } else if (location.type === 'rail') {
+      el.classList.add([ 'train' ]);
+    } else {
+      el.style.backgroundColor = '#bbb';
+    }
+    let marker = new mapboxgl.Marker(el);
+    marker.setLngLat([location.longitude, location.latitude]);
+    marker.addTo(map);
+    mapLocations.push(marker);
+  });
+}
+
+ws.onmessage = function (event) {
+  moveMarkers(event);
+};
